70c47b921664654527e80aece1cbe432be5bba8f
huangguorong <huangguorong@18d54d2b-35bc-4a19-92ed-a673819ede82>
2015-05-07 03:40:17 +0000

    形象进度推进添加编辑权限3

--- serverside/java/src/main/java/com/justonetech/system/utils/PrivilegeCode.java
+++ serverside/java/src/main/java/com/justonetech/system/utils/PrivilegeCode.java
@@ -63,5 +63,10 @@ public class PrivilegeCode {
     public static final String PROJ_STAGE_EDIT = ProjStage.class.getSimpleName() + "_edit";//项目办证阶段管理_编辑
     public static final String PROJ_NODE_EDIT = ProjNode.class.getSimpleName() + "_edit";//形象进度节点管理_编辑
     public static final String PROJ_RELATE_DEPT = ProjRelateDept.class.getSimpleName() + "_edit";//建设单位管理_编辑
+    /**
+     * ***************************************项目管理******************************************************
+     */
+    public static final String DATA_NODE_REPORT_EDIT = DataNodeReport.class.getSimpleName() + "_edit";//形象进度推进填报_编辑
+
 }
 
