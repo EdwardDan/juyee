efe77a6c599389e48c7f526c46c87919a85683c1
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-06-29 02:28:18 +0000

    创建科室一周工作portlet

---
+++ liferay/trunk/oa-portlet/docroot/WEB-INF/src/com/justonetech/oa/portlet/DeptWeeklyWorkPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.oa.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DeptWeeklyWorkPortlet
+ */
+public class DeptWeeklyWorkPortlet extends MVCPortlet {
+ 
+
+}
