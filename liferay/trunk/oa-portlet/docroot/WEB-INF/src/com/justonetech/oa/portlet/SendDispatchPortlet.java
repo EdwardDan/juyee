fee8c8654ede78c1a444bdbc61fd722290eea708
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-06-30 10:43:23 +0000

    创建发文管理portlet

---
+++ liferay/trunk/oa-portlet/docroot/WEB-INF/src/com/justonetech/oa/portlet/SendDispatchPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.oa.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class SendDispatchPortlet
+ */
+public class SendDispatchPortlet extends MVCPortlet {
+ 
+
+}
---
+++ liferay/trunk/oa-portlet/docroot/portlet/send-dispatch/view.jsp
@@ -0,0 +1,5 @@
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<portlet:defineObjects />
+
+This is the <b>发文管理</b> portlet in View mode.
