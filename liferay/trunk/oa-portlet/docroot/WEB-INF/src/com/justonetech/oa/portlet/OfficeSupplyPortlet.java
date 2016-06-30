26dbdde9fd5b7a6889e636c16f2832502b1a9823
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-06-30 01:24:53 +0000

    调整异常处理代码

--- liferay/trunk/oa-portlet/docroot/WEB-INF/src/com/justonetech/oa/portlet/OfficeSupplyPortlet.java
+++ liferay/trunk/oa-portlet/docroot/WEB-INF/src/com/justonetech/oa/portlet/OfficeSupplyPortlet.java
@@ -1,3 +1,4 @@
+
 package com.justonetech.oa.portlet;
 
 import java.io.IOException;
@@ -31,131 +32,132 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  * Portlet implementation class OfficeSupplyPortlet
  */
 public class OfficeSupplyPortlet extends MVCPortlet {
-	private static Log log = LogFactoryUtil.getLog(OfficeSupplyPortlet.class);
 
-	@Override
-	public void doView(RenderRequest renderRequest,
-			RenderResponse renderResponse) throws IOException, PortletException {
-		String keywords = ParamUtil.getString(renderRequest, "keywords");
-		int delta = GetterUtil.getInteger(PropsUtil
-				.get(PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA));
-		int pageSize = ParamUtil.getInteger(renderRequest, "delta", delta);
-		int pageNumber = ParamUtil.getInteger(renderRequest, "cur", 1);
-		int start = pageSize * (pageNumber - 1);
-		int end = pageSize * pageNumber;
+    private static Log log = LogFactoryUtil.getLog(OfficeSupplyPortlet.class);
 
-		List<OfficeSupply> officeSupplies = Collections.emptyList();
-		try {
-			officeSupplies = OfficeSupplyLocalServiceUtil
-					.getOfficeSuppliesByName(keywords, start, end);
-		} catch (SystemException e) {
-			log.error("getOfficeSuppliesByName(" + keywords + ", " + start
-					+ ", " + end + ")");
-			e.printStackTrace();
-		}
-		int officeSuppliesCount = 0;
-		try {
-			officeSuppliesCount = OfficeSupplyLocalServiceUtil
-					.getOfficeSuppliesCountByName(keywords);
-		} catch (SystemException e) {
-			log.error("getOfficeSuppliesCountByName(" + keywords + ")");
-			e.printStackTrace();
-		}
-		renderRequest.setAttribute("officeSupplies", officeSupplies);
-		renderRequest.setAttribute("officeSuppliesCount", officeSuppliesCount);
+    @Override
+    public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
+        throws IOException, PortletException {
 
-		super.doView(renderRequest, renderResponse);
-	}
+        String keywords = ParamUtil.getString(renderRequest, "keywords");
+        int delta = GetterUtil.getInteger(PropsUtil.get(PropsKeys.SEARCH_CONTAINER_PAGE_DEFAULT_DELTA));
+        int pageSize = ParamUtil.getInteger(renderRequest, "delta", delta);
+        int pageNumber = ParamUtil.getInteger(renderRequest, "cur", 1);
+        int start = pageSize * (pageNumber - 1);
+        int end = pageSize * pageNumber;
 
-	public void editOfficeSupply(ActionRequest actionRequest,
-			ActionResponse actionResponse) throws IOException, PortletException {
-		long officeSupplyId = ParamUtil
-				.getLong(actionRequest, "officeSupplyId");
-		OfficeSupply officeSupply = null;
-		if (officeSupplyId != 0) {
-			try {
-				officeSupply = OfficeSupplyLocalServiceUtil
-						.getOfficeSupply(officeSupplyId);
-				log.error("getOfficeSuppliesCountByName(" + officeSupplyId
-						+ ")");
-			} catch (PortalException | SystemException e) {
-				e.printStackTrace();
-			}
-		}
-		actionRequest.setAttribute("officeSupply", officeSupply);
-	}
+        List<OfficeSupply> officeSupplies = Collections.emptyList();
+        try {
+            officeSupplies = OfficeSupplyLocalServiceUtil.getOfficeSuppliesByName(keywords, start, end);
+        }
+        catch (SystemException e) {
+            log.error("getOfficeSuppliesByName(" + keywords + ", " + start + ", " + end + ")");
+            e.printStackTrace();
+        }
+        int officeSuppliesCount = 0;
+        try {
+            officeSuppliesCount = OfficeSupplyLocalServiceUtil.getOfficeSuppliesCountByName(keywords);
+        }
+        catch (SystemException e) {
+            log.error("getOfficeSuppliesCountByName(" + keywords + ")");
+            e.printStackTrace();
+        }
+        renderRequest.setAttribute("officeSupplies", officeSupplies);
+        renderRequest.setAttribute("officeSuppliesCount", officeSuppliesCount);
 
-	public void saveOfficeSupply(ActionRequest actionRequest,
-			ActionResponse actionResponse) throws IOException, PortletException {
-		long officeSupplyId = ParamUtil
-				.getLong(actionRequest, "officeSupplyId");
-		String name = ParamUtil.getString(actionRequest, "name");
-		String model = ParamUtil.getString(actionRequest, "model");
-		String unit = ParamUtil.getString(actionRequest, "unit");
-		double unitPrice = ParamUtil.getDouble(actionRequest, "unitPrice");
-		int quantity = ParamUtil.getInteger(actionRequest, "quantity");
-		OfficeSupply officeSupply = null;
-		Date now = new Date();
-		if (officeSupplyId == 0) {
-			try {
-				officeSupplyId = CounterLocalServiceUtil.increment(
-						"officeSupply", 1);
-			} catch (SystemException e) {
-				e.printStackTrace();
-			}
-			officeSupply = OfficeSupplyLocalServiceUtil
-					.createOfficeSupply(officeSupplyId);
+        super.doView(renderRequest, renderResponse);
+    }
 
-			officeSupply.setCreateTime(now);
-		} else {
-			try {
-				officeSupply = OfficeSupplyLocalServiceUtil
-						.getOfficeSupply(officeSupplyId);
-			} catch (PortalException | SystemException e) {
-				e.printStackTrace();
-			}
-		}
+    public void editOfficeSupply(ActionRequest actionRequest, ActionResponse actionResponse)
+        throws IOException, PortletException {
 
-		User user = null;
-		try {
-			user = PortalUtil.getUser(actionRequest);
-		} catch (PortalException | SystemException e) {
+        long officeSupplyId = ParamUtil.getLong(actionRequest, "officeSupplyId");
+        OfficeSupply officeSupply = null;
+        if (officeSupplyId != 0) {
+            try {
+                officeSupply = OfficeSupplyLocalServiceUtil.getOfficeSupply(officeSupplyId);
+                log.error("getOfficeSuppliesCountByName(" + officeSupplyId + ")");
+            }
+            catch (PortalException | SystemException e) {
+                e.printStackTrace();
+            }
+        }
+        actionRequest.setAttribute("officeSupply", officeSupply);
+    }
 
-		}
-		if (Validator.isNotNull(user)) {
-			officeSupply.setUserId(user.getUserId());
-			officeSupply.setUserName(user.getFullName());
-		}
+    public void saveOfficeSupply(ActionRequest actionRequest, ActionResponse actionResponse)
+        throws IOException, PortletException {
 
-		officeSupply.setName(name);
-		officeSupply.setModel(model);
-		officeSupply.setUnit(unit);
-		officeSupply.setUnitPrice(unitPrice);
-		officeSupply.setQuantity(quantity);
-		officeSupply.setModifiedTime(now);
+        long officeSupplyId = ParamUtil.getLong(actionRequest, "officeSupplyId");
+        String name = ParamUtil.getString(actionRequest, "name");
+        String model = ParamUtil.getString(actionRequest, "model");
+        String unit = ParamUtil.getString(actionRequest, "unit");
+        double unitPrice = ParamUtil.getDouble(actionRequest, "unitPrice");
+        int quantity = ParamUtil.getInteger(actionRequest, "quantity");
+        OfficeSupply officeSupply = null;
+        Date now = new Date();
+        if (officeSupplyId == 0) {
+            try {
+                officeSupplyId = CounterLocalServiceUtil.increment();
+            }
+            catch (SystemException e) {
+                e.printStackTrace();
+            }
+            officeSupply = OfficeSupplyLocalServiceUtil.createOfficeSupply(officeSupplyId);
 
-		try {
-			OfficeSupplyLocalServiceUtil.updateOfficeSupply(officeSupply);
-			log.error("updateOfficeSupply error:" + officeSupply.toString());
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}
-	}
+            officeSupply.setCreateTime(now);
+        }
+        else {
+            try {
+                officeSupply = OfficeSupplyLocalServiceUtil.getOfficeSupply(officeSupplyId);
+            }
+            catch (PortalException | SystemException e) {
+                e.printStackTrace();
+            }
+        }
 
-	public void deleteOfficeSupplies(ActionRequest actionRequest,
-			ActionResponse actionResponse) throws IOException, PortletException {
-		String deleteOfficeSupplyIds = ParamUtil.getString(actionRequest,
-				"officeSupplyIds");
-		String[] officeSupplyIds = deleteOfficeSupplyIds.split(",");
-		for (String officeSupplyId : officeSupplyIds) {
-			try {
-				OfficeSupplyLocalServiceUtil.deleteOfficeSupply(Long
-						.parseLong(officeSupplyId));
-			} catch (NumberFormatException | PortalException | SystemException e) {
-				log.error("deleteOfficeSupply(" + officeSupplyId + ")");
-				e.printStackTrace();
-			}
-		}
-	}
+        User user = null;
+        try {
+            user = PortalUtil.getUser(actionRequest);
+        }
+        catch (PortalException | SystemException e) {
+
+        }
+        if (Validator.isNotNull(user)) {
+            officeSupply.setUserId(user.getUserId());
+            officeSupply.setUserName(user.getFullName());
+        }
+
+        officeSupply.setName(name);
+        officeSupply.setModel(model);
+        officeSupply.setUnit(unit);
+        officeSupply.setUnitPrice(unitPrice);
+        officeSupply.setQuantity(quantity);
+        officeSupply.setModifiedTime(now);
+
+        try {
+            OfficeSupplyLocalServiceUtil.updateOfficeSupply(officeSupply);
+        }
+        catch (SystemException e) {
+            log.error("updateOfficeSupply error:" + officeSupply.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteOfficeSupplies(ActionRequest actionRequest, ActionResponse actionResponse)
+        throws IOException, PortletException {
+
+        String deleteOfficeSupplyIds = ParamUtil.getString(actionRequest, "officeSupplyIds");
+        String[] officeSupplyIds = deleteOfficeSupplyIds.split(",");
+        for (String officeSupplyId : officeSupplyIds) {
+            try {
+                OfficeSupplyLocalServiceUtil.deleteOfficeSupply(Long.parseLong(officeSupplyId));
+            }
+            catch (NumberFormatException | PortalException | SystemException e) {
+                log.error("deleteOfficeSupply(" + officeSupplyId + ")");
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
