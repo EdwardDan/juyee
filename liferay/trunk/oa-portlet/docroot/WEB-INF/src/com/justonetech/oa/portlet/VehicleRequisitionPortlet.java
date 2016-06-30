92dc893fad09a22ef5f57bd05cee2dd763c6cc24
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-06-30 11:30:34 +0000

    创建车辆申请portlet

---
+++ liferay/trunk/oa-portlet/docroot/WEB-INF/src/com/justonetech/oa/portlet/VehicleRequisitionPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.oa.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class VehicleRequisitionPortlet
+ */
+public class VehicleRequisitionPortlet extends MVCPortlet {
+ 
+
+}
