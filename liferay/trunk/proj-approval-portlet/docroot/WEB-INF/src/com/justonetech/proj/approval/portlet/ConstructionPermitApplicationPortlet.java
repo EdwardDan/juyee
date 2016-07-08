0710ba78df6dc27059880e1d35c8cbb59b4dc804
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-07-08 02:24:08 +0000

    创建施工许可申请portlet及值对象包

---
+++ liferay/trunk/proj-approval-portlet/docroot/WEB-INF/src/com/justonetech/proj/approval/portlet/ConstructionPermitApplicationPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.proj.approval.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ConstructionPermitApplicationPortlet
+ */
+public class ConstructionPermitApplicationPortlet extends MVCPortlet {
+ 
+
+}
