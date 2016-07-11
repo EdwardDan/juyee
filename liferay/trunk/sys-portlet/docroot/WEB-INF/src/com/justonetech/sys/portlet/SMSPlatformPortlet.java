daaddba45adf242e3625820da9b941854c725e90
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-07-11 05:25:08 +0000

    创建短信平台portlet

---
+++ liferay/trunk/sys-portlet/docroot/WEB-INF/src/com/justonetech/sys/portlet/SMSPlatformPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.sys.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class SMSPlatformPortlet
+ */
+public class SMSPlatformPortlet extends MVCPortlet {
+ 
+
+}
