3a4a26c4ada68088daa6542d471f556af472bc26
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-08-25 04:57:04 +0000

    建设程序

---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-portlet/docroot/WEB-INF/src/com/justonetech/portlet/constructionproject/ConstructionProjectPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.portlet.constructionproject;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ConstructionProjectPortlet
+ */
+public class ConstructionProjectPortlet extends MVCPortlet {
+ 
+
+}
---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-portlet/docroot/common/init-ext.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@page import="com.liferay.portal.kernel.util.PropsUtil"%>
