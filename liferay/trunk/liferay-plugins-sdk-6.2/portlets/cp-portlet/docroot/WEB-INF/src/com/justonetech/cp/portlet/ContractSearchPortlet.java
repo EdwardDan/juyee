526fd2cc72346915f8e93b3dcc55dce561fe7985
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-08-30 02:33:12 +0000

    创建portlet

---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-portlet/docroot/WEB-INF/src/com/justonetech/cp/portlet/ContractSearchPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.cp.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ContractSearchPortlet
+ */
+public class ContractSearchPortlet extends MVCPortlet {
+ 
+
+}
---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-portlet/docroot/WEB-INF/src/com/justonetech/cp/portlet/PermitApplicationPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.cp.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PermitApplicationPortlet
+ */
+public class PermitApplicationPortlet extends MVCPortlet {
+ 
+
+}
