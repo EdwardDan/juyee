7bdc3782493513d4a67e9deda34c9879f964ad3b
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-09-01 01:33:50 +0000

    已报送施工总包合同相关字段及查询方法

--- liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-service-portlet/docroot/WEB-INF/src/com/justonetech/cp/contract/service/impl/ContractLocalServiceImpl.java
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-service-portlet/docroot/WEB-INF/src/com/justonetech/cp/contract/service/impl/ContractLocalServiceImpl.java
@@ -20,36 +20,38 @@ import java.util.List;
 import com.justonetech.cp.contract.model.Contract;
 import com.justonetech.cp.contract.service.base.ContractLocalServiceBaseImpl;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Junction;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 /**
- * The implementation of the contract local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.justonetech.cp.contract.service.ContractLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
+ * The implementation of the contract local service. <p> All custom service
+ * methods should be put in this class. Whenever methods are added, rerun
+ * ServiceBuilder to copy their definitions into the
+ * {@link com.justonetech.cp.contract.service.ContractLocalService} interface.
+ * <p> This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM. </p>
  *
  * @author fanqi
  * @see com.justonetech.cp.contract.service.base.ContractLocalServiceBaseImpl
  * @see com.justonetech.cp.contract.service.ContractLocalServiceUtil
  */
 public class ContractLocalServiceImpl extends ContractLocalServiceBaseImpl {
+
 	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.justonetech.cp.contract.service.ContractLocalServiceUtil} to access the contract local service.
+	 * NOTE FOR DEVELOPERS: Never reference this interface directly. Always use
+	 * {@link com.justonetech.cp.contract.service.ContractLocalServiceUtil} to
+	 * access the contract local service.
 	 */
 	private static Log log = LogFactoryUtil.getLog(ContractLocalServiceImpl.class);
 
 	@SuppressWarnings("unchecked")
-	public List<Contract> getContracts(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx,String htxxbsbh, int start, int end) {
+	public List<Contract> getContracts(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx, String htxxbsbh, int start, int end) {
 
 		try {
 			return this.dynamicQuery(createDynamicQuery(zzjgdm, bjbh, bdh, xmmc, htlx, htxxbsbh), start, end);
@@ -60,7 +62,7 @@ public class ContractLocalServiceImpl extends ContractLocalServiceBaseImpl {
 		return Collections.emptyList();
 	}
 
-	public int getContractsCount(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx,String htxxbsbh) {
+	public int getContractsCount(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx, String htxxbsbh) {
 
 		try {
 			return (int) this.dynamicQueryCount(createDynamicQuery(zzjgdm, bjbh, bdh, xmmc, htlx, htxxbsbh));
@@ -71,7 +73,7 @@ public class ContractLocalServiceImpl extends ContractLocalServiceBaseImpl {
 		return 0;
 	}
 
-	public DynamicQuery createDynamicQuery(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx,String htxxbsbh) {
+	public DynamicQuery createDynamicQuery(String zzjgdm, String bjbh, String bdh, String xmmc, String htlx, String htxxbsbh) {
 
 		DynamicQuery dynamicQuery = this.dynamicQuery();
 		if (!Validator.isBlank(zzjgdm)) {
@@ -92,6 +94,50 @@ public class ContractLocalServiceImpl extends ContractLocalServiceBaseImpl {
 		if (!Validator.isBlank(htxxbsbh)) {
 			dynamicQuery.add(PropertyFactoryUtil.forName("htxxbsbh").like("%" + htxxbsbh + "%"));
 		}
+		return dynamicQuery;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Contract> getYbssgzbContracts(String zzjgdm, String bjbh, String bdh, String htmc, int start, int end) {
+
+		try {
+			return this.dynamicQuery(createYbssgzbDQ(zzjgdm, bjbh, bdh, htmc), start, end);
+		}
+		catch (SystemException e) {
+			log.info(e.getMessage());
+		}
+		return Collections.emptyList();
+	}
+
+	public int getYbssgzbContractsCount(String zzjgdm, String bjbh, String bdh, String htmc) {
+
+		try {
+			return (int) this.dynamicQueryCount(createYbssgzbDQ(zzjgdm, bjbh, bdh, htmc));
+		}
+		catch (SystemException e) {
+			log.info(e.getMessage());
+		}
+		return 0;
+	}
+
+	public DynamicQuery createYbssgzbDQ(String zzjgdm, String bjbh, String bdh, String htmc) {
+
+		DynamicQuery dynamicQuery = this.dynamicQuery();
+		if (!Validator.isBlank(zzjgdm)) {
+			dynamicQuery.add(PropertyFactoryUtil.forName("zzjgdm").eq(zzjgdm));
+		}
+		if (!Validator.isBlank(bjbh)) {
+			dynamicQuery.add(PropertyFactoryUtil.forName("bjbh").like("%" + bjbh + "%"));
+		}
+		if (!Validator.isBlank(bdh)) {
+			dynamicQuery.add(PropertyFactoryUtil.forName("bdh").like("%" + bdh + "%"));
+		}
+		dynamicQuery.add(PropertyFactoryUtil.forName("bdh").like("V%"));
+		if (!Validator.isBlank(htmc)) {
+			dynamicQuery.add(PropertyFactoryUtil.forName("htmc").like("%" + htmc + "%"));
+		}
+		dynamicQuery.add(PropertyFactoryUtil.forName("cbfs").eq("总包"));
+		dynamicQuery.add(PropertyFactoryUtil.forName("htzt").eq("已报送"));
 		return dynamicQuery;
 	}
 }
