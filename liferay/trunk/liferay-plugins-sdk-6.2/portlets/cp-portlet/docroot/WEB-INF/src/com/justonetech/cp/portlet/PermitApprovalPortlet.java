191084a2a6a375a1e04635f51c78791097536196
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-09-05 07:07:46 +0000

    施工许可审核portlet

---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/cp-portlet/docroot/WEB-INF/src/com/justonetech/cp/portlet/PermitApprovalPortlet.java
@@ -0,0 +1,10 @@
+package com.justonetech.cp.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PermitApprovalPortlet
+ */
+public class PermitApprovalPortlet extends MVCPortlet {
+ 
+}
