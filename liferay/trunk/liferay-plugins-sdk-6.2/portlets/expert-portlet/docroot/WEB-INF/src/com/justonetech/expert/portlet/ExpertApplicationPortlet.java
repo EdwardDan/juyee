f0b0894d2a1d3e226d947fe5384396693ac2cd3a
fanqi <fanqi@18d54d2b-35bc-4a19-92ed-a673819ede82>
2016-11-24 06:15:41 +0000

    expert portlet

---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/expert-portlet/docroot/WEB-INF/src/com/justonetech/expert/portlet/ExpertApplicationPortlet.java
@@ -0,0 +1,11 @@
+package com.justonetech.expert.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ExpertApplicationPortlet
+ */
+public class ExpertApplicationPortlet extends MVCPortlet {
+ 
+
+}
---
+++ liferay/trunk/liferay-plugins-sdk-6.2/portlets/expert-portlet/docroot/portlet/expert-approval/view.jsp
@@ -0,0 +1,5 @@
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<portlet:defineObjects />
+
+This is the <b>专家审批</b> portlet in View mode.
